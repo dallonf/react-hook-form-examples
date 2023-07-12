7b7efe7f03ce93b06ab44a0a0a7757a43a05f16d
Dallon Feldner <dallon@sibipro.com>
2023-07-12 12:04:41 -0500

    Example of form validation

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,17 +1,25 @@
 import { cn } from "@/utils/classNames";
 import React from "react";
 
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  errorState?: boolean;
+}
+
 export const Input = React.forwardRef(
   Object.assign(
-    (
-      props: React.InputHTMLAttributes<HTMLInputElement>,
-      ref: React.Ref<HTMLInputElement>
-    ) => {
-      const { className, ...rest } = props;
+    (props: InputProps, ref: React.Ref<HTMLInputElement>) => {
+      const { className, errorState, ...rest } = props;
       return (
         <input
           ref={ref}
-          className={cn("border border-gray-300 p-2 rounded-md", className)}
+          className={cn(
+            "border border-gray-300 p-2 rounded-md",
+            {
+              "border-red-500": errorState,
+            },
+            className
+          )}
           {...rest}
         />
       );
---
+++ src/pages/02-basic-form-validation.tsx
@@ -0,0 +1,69 @@
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import clsx from "clsx";
+import { useState } from "react";
+
+interface FormState {
+  email: string;
+  favoriteFood: string;
+}
+
+const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+export default function BasicFormPage() {
+  const [formState, setFormState] = useState<FormState>({
+    email: "",
+    favoriteFood: "",
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(JSON.stringify(formState, null, 2));
+  };
+
+  let emailError: string | undefined = undefined;
+  if (!formState.email.length) {
+    emailError = "Email is required";
+  } else if (formState.email.match(EMAIL_REGEX) === null) {
+    emailError = "Email must be a valid format";
+  }
+
+  return (
+    <main className="p-2">
+      <form onSubmit={handleSubmit}>
+        <label
+          htmlFor="email"
+          className={clsx("mr-3", {
+            "text-red-500": emailError,
+          })}
+        >
+          Email
+        </label>
+        <Input
+          type="text"
+          id="email"
+          errorState={Boolean(emailError)}
+          value={formState.email}
+          onChange={(e) =>
+            setFormState((prev) => ({ ...prev, email: e.target.value }))
+          }
+        />
+        {emailError && <span className="text-red-500 ml-2">{emailError}</span>}
+        <br />
+        <label htmlFor="favoriteFood" className="mr-3">
+          Favorite Food
+        </label>
+        <Input
+          type="text"
+          id="favoriteFood"
+          value={formState.favoriteFood}
+          onChange={(e) =>
+            setFormState((prev) => ({ ...prev, favoriteFood: e.target.value }))
+          }
+        />
+        <br />
+        <Button type="submit">Submit</Button>
+      </form>
+    </main>
+  );
+}
